5379505c5c8a517c19bdd37793ef78f40d6534a8
Enric Vergara <donkikochan@gmail.com>
2023-10-11 10:13:57 +0200

    setup bomb prefab

--- Assets/Scripts/BombScript.cs
+++ Assets/Scripts/BombScript.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BombScript : MonoBehaviour
 {
-    private GameObject gm;
-    // Start is called before the first frame update
+    public float timeToExplosion = 4.0f;
+    private float timer = 0.0f;
+    private GameManager gm = null;
+    
     void Start()
     {
-        gm = GameObject.FindGameObjectWithTag("GameManager");
+        GameObject o = GameObject.FindGameObjectWithTag("GameManager");
         
-        if (gm != null)
+        if (o == null)
         {
-            Debug.LogError("No hi ha cap gameObject amb tag GameManahger");
+            Debug.LogError("There's no gameObject with GameManager tag.");
+        }
+        else
+        {
+            gm = o.GetComponent<GameManager>();
+            if (gm == null)
+            {
+                Debug.LogError("The GameObject with GameManager tag doesn't have the GameManager script attached to it");
+            }
         }
     }
 
@@ -20,12 +31,20 @@ public class BombScript : MonoBehaviour
     void Update()
     {
         //Si han passat 4 segons --> Destroy this gameObject & damage using GameManeger:
-        
-            
+        timer += Time.deltaTime;
+        if (timer > timeToExplosion)
+        {
+            gm.TakeDamage();
+            Destroy(gameObject);
+        }
+
     }
     
     //Si l'usuari fa click sobre la bomba=>
     // Destroy
-    
-    
+    private void OnMouseDown()
+    {
+        gm.AddScore();
+        Destroy(gameObject);
+    }
 }
